c5f540d1c20cdce477b4fa03b8b01b37ced8f761
Lucas M. Rodrigues <lucas@scienceweb.com.br>
2022-06-07 17:03:22 -0300

    Alteração settings.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from python_decouple import config
+from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
